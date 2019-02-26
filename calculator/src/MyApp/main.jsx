fc6ce8b3607189c94eae109bc2dab4202ece4673
ami gilbert <ami.gilbert@hotmail.com>
2019-02-25 21:00:24 -0700

    1st commit

---
+++ calculator/src/MyApp/main.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './style.css'
+
+class Main extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Two Number addition</h1>
+            </div>
+        )
+    }
+}
+
+export default Main
